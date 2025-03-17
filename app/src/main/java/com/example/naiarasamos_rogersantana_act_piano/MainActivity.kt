d5dfe4286147c92ceaf2cbe986f5b0c0cadf6bfa
Naiara <a>
2025-03-17 16:54:50 +0100

    Comentario y Función para mapear las teclas y no tener todo en el OnCreate.

--- app/src/main/java/com/example/naiarasamos_rogersantana_act_piano/MainActivity.kt
+++ app/src/main/java/com/example/naiarasamos_rogersantana_act_piano/MainActivity.kt
@@ -13,8 +13,9 @@ import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var soundPool: SoundPool
-    private val soundMap = mutableMapOf<Int, Int>()
+    private lateinit var soundPool: SoundPool // soundPool para manejar sonidos.
+    private val soundMap = mutableMapOf<Int, Int>() // Mapa de sonidos y teclas.
+    private lateinit var keys: Map<Int, Int> // Variable que contiene las teclas (botones)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,8 +31,20 @@ class MainActivity : AppCompatActivity() {
             )
             .build()
 
+        mapKeys() // Llamamos a la función que mapea cada sonido con la tecla correspondiente.
+
+        // Se añade cada sonido al SoundPool
+        keys.forEach {
+                (key, sound) -> soundMap[key] = soundPool.load(this, sound, 1)
+        }
+
+        // Se asigna el listener a cada botón
+        keys.keys.forEach { setKeyListener(it) }
+    }
+
+    private fun mapKeys(){
         // Cargar sonidos en el mapa
-        val keys = mapOf(
+        keys = mapOf(
             R.id.key_c2 to R.raw.c2, R.id.key_d2 to R.raw.d2, R.id.key_e2 to R.raw.e2,
             R.id.key_f2 to R.raw.f2, R.id.key_g2 to R.raw.g2, R.id.key_a2 to R.raw.a2, R.id.key_b2 to R.raw.b2,
 
@@ -43,14 +56,10 @@ class MainActivity : AppCompatActivity() {
             R.id.key_c_3 to R.raw.c_3, R.id.key_d_2 to R.raw.d_2, R.id.key_d_3 to R.raw.d_3,
             R.id.key_f_2 to R.raw.f_2, R.id.key_f_3 to R.raw.f_3, R.id.key_g_2 to R.raw.g_2, R.id.key_g_3 to R.raw.g_3
         )
-
-        // Se añade cada sonido al SoundPool
-        keys.forEach {
-            (key, sound) -> soundMap[key] = soundPool.load(this, sound, 1)
-        }
-        // Se asigna el listener a cada botón
-        keys.keys.forEach { setKeyListener(it) }
     }
+
+    // Función que contiene el listener de cada botón para que suene al recibir el action_down.
+    // Al usar el StateListDrawable, no necesitamos el action_up.
     @SuppressLint("ClickableViewAccessibility", "ResourceAsColor")
     private fun setKeyListener(keyId: Int) {
         val key = findViewById<ImageButton>(keyId)
